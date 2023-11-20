7c6c24032cc098304622f425b7f985969e2ff254
Gil <gsqsanchez@gmail.com>
2023-11-19 21:55:37 -0700

    Add home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './components/Home';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <div className='App'>
+        <Nav/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
