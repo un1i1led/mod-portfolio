3a5ff1c5033d085bbdeef57aeb46bc0d676cf700
Gil <gsqsanchez@gmail.com>
2023-12-01 17:12:48 -0700

    Add project and contact route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './components/Home';
 import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
 
@@ -15,11 +17,13 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/contact' element={<Contact/>}/>
         </Routes>
         <div className='footer'>
             <div className='footer-imgs'>
-                <img src={githubWhite} alt='github link'/>
-                <img src={linkedin} alt='linkedin link'/>
+                <a href='https://www.github.com/un1i1led' className='no-style'><img src={githubWhite} alt='github link'/></a>
+                <a href='https://www.linkedin.com/in/gilsanquin/'><img src={linkedin} alt='linkedin link'/></a>
             </div>
             <p>© 2023 • Gilberto Sanchez. All Rights Reserved.</p>
         </div>
