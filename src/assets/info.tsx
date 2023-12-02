760ad202c1d698c38b3893ff9a4ae8138fe0c14a
Gil <gsqsanchez@gmail.com>
2023-12-01 17:13:19 -0700

    Add project card info

---
+++ src/assets/info.tsx
@@ -0,0 +1,11 @@
+import blog from '../assets/blogApp.png';
+import cv from '../assets/cvApp.png';
+import weather from '../assets/weather.png';
+
+const projectList = [
+    { image: blog, name: 'Blogging App', description: 'I developed a blog application from scratch using moderntechnologies to deliver a dynamic and engaging user experience. The application is based on a client-server architecture, where the frontend was built using React, while the backend was implemented with Express, following a RESTful architecture, and connected to a MongoDB database for data persistence.', live: 'https://un1i1led.github.io/blogFront', git: 'https://github.com/un1i1led/blogFront' },
+    { image: cv, name: 'CV Creator', description: 'Designed and developed a dynamic CV builder using React, HTML and CSS, allowing users to create personalized resumes effortlessly', live: 'https://un1i1led.github.io/cv-project', git: 'https://github.com/un1i1led/cv-project' },
+    { image: weather, name: 'Weather App', description: 'An application where you use Javascript and APIs to provide instant access to real-time weather information for cities around the world.', live: 'https://un1i1led.github.io/weatherApp', git: 'https://github.com/un1i1led/weatherApp' }
+]
+
+export default projectList;
